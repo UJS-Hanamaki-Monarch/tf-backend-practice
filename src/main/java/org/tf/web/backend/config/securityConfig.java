0d758d95042a4267e59714229865e2dcf8851c98
Chuck <wyy453822487@outlook.com>
2022-02-02 18:23:49 +0800

    feat:  format Class name

--- src/main/java/org/tf/web/backend/config/securityConfig.java
+++ src/main/java/org/tf/web/backend/config/securityConfig.java
@@ -10,7 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  */
 
 @Configuration
-public class securityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/**").permitAll() // 放行所有权限
